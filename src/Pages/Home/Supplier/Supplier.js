e728a8907b39841415c3ff88fc9f823bdf01eb8c
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 19:13:44 +0600

    home page extra section added

--- src/Pages/Home/Supplier/Supplier.js
+++ src/Pages/Home/Supplier/Supplier.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Supplier = ({info}) => {
     return (
-     
+        <div className='col gy-4 gx-2 shadow rounded p-3 mb-2 bg-body'>
+        <div className="card shadow rounded mb-2 bg-body">
+            
+            <div className="card-body">
+                <h5 className="card-title">{info.name}</h5>
+                <p className="">Address : {info.address} </p>
+                <p className="">COntact No. : {info.mobile} </p>
+            </div>
+            {/* <div className="card-footer d-block text-center">
+                <button onClick={() => handleStockUpdate(_id)} className='btn btn-info '> Stock Update</button>
+            </div> */}
+        </div>
+    </div>
     );
 };
 
