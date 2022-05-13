be8df99b617269d83df49a8c468ed793cacab439
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 18:53:32 +0600

    header section

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -17,7 +17,45 @@ const Header = () => {
 
     return (
         <>
-            
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="warning" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={30} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto fw-bold">
+                        <Nav.Link as={Link} to="home">Home</Nav.Link>
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            
+                            <Nav.Link as={Link} to="Blog">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            
+                            
+                        </Nav>
+                        <Nav className='fw-bold'>
+
+                         {
+                                user && 
+                                <>
+        
+                                <Nav.Link as={Link} to="AddItem">Add Item</Nav.Link>
+                                <Nav.Link as={Link} to="manage">Manage Products</Nav.Link>
+                                <Nav.Link as={Link} to="myItem">My Item</Nav.Link>
+                                </>
+                         }
+                            
+                            {
+                                user ?
+                                    <button className='btn btn-link text-danger fw-bold text-decoration-none' onClick={handleSignOut}>SIGN OUT !!! </button>
+                                :
+                                <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     );
 };
