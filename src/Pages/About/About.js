669b5ee8fce5f1cd094af2ade48c414b13bd6fe2
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 18:50:53 +0600

    about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,6 +4,46 @@ import './About.css'
 const About = () => {
     return (
         
+<div className='container   
+        shadow rounded mt-4 mb-4'>
+            <div className='shadow rounded'>
+                
+                <h1>
+                The gym equipment market, industry trends and updates  
+
+                </h1>
+                
+                <p>
+            The global fitness equipment market was $11.6 billion in 2020. It’s estimated to reach $14.8 billion by 2028. The fitness equipment market includes machines and devices for physical exercise. The market is evolving at a good speed with an increasing awareness of health and fitness among consumers. 
+
+The COVID-19 outbreak has had a positive impact on the fitness equipment market. People are becoming more health-conscious and gym equipment suppliers are looking for ways to cater their gym equipment to home fitness users. Consumers have a newfound awareness for the way that fitness supports mental, physical, and emotional well-being. 
+
+When you break down the gym equipment market into segments, you can see how the home fitness equipment has erupted over the last year. Research shows that the home gym equipment market is estimated to rise at a considerable rate from 2021 to 2027. 
+
+If you look at brands that have found another level of success during the pandemic, the themes tend to be centered around home fitness equipment. For instance, Nautilus, Inc finished the year with record-breaking sales. Sales growth was driven by increased demand for connected bikes and sales of Bowflex Home Gyms. No doubt, this will be an area of gym equipment that will continue to create a lot of business opportunities and innovation within the fitness market. 
+                
+                </p></div>
+            <div className='shadow rounded my-4'>
+                <h1>What are the different types of fitness equipment?</h1>
+
+                <p>
+                The world of gym equipment is vast. There are a lot of different types of fitness equipment with various features and connectivity like a touchscreen and streaming abilities. You can go from a no-frills treadmill to a smart rower with full connectivity. Your gym equipment is a huge part of the member experience. It creates a level of convenience and allows members to hit their fitness goals. With that in mind, let’s look at the various machines and gym equipment you can purchase. 
+                </p>
+            </div>
+
+
+            <div className='shadow rounded my-4'>
+                <h1  className=' m-4'>Should you buy used or brand-new gym equipment?</h1>
+
+                <p>
+                Buying gym equipment is challenging. With so many different brands, names, and types of equipment, it takes time and effort to make the right choice for your business. As fitness trends come and go and the price of gym equipment increases, it can be a costly mistake. Apart from the different categories of workout equipment, you also have used or brand-new fitness equipment. While some businesses prefer used equipment, others will only buy brand new. So, should you buy brand new or used gym equipment? 
+
+With skyrocketing prices of gym equipment, used equipment is generally available at a much cheaper price. However, it’s not as simple as buying used equipment for a cheaper price and then leaving it there. Sometimes, used equipment can end up more expensive in the long term because it has had a lot more wear and tear. You also need to consider hygiene issues, how easily you can obtain parts for repair and potential repair costs. 
+
+The main downside of new equipment is the higher cost. But, the initial investment can save you money in the future as repair costs should be lower. When buying new equipment, look for a full warranty package with no hidden catches. Trusted brands and retailers should supply machines with years of good use. 
+                </p>
+            </div>
+        </div>
     );
 };
 
