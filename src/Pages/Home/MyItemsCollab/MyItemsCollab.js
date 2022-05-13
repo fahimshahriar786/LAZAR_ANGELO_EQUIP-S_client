6a34d726deb1fee359b2112ef58f14943cca684c
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 19:08:41 +0600

    my items collabs

--- src/Pages/Home/MyItemsCollab/MyItemsCollab.js
+++ src/Pages/Home/MyItemsCollab/MyItemsCollab.js
@@ -3,7 +3,9 @@ import React from 'react';
 const MyItemsCollab = ({myproduct,handleUserDelete}) => {
     const { name, _id, price, img, description, quantity, supplier } = myproduct
     return (
-       
+        <div className='col gy-4 gx-2 shadow rounded p-3 mb-2 bg-body'>
+        <div className="card shadow rounded mb-2 bg-body">
+            <img src={img} style={{ "height": "300px" }} className=" w-100 card-img-top" alt="..." />
     );
 };
 
