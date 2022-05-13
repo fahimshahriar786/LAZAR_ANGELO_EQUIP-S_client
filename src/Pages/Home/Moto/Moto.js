c86026ee77db0a236dae5b02950252b09e9eed69
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 19:05:49 +0600

    home page moto section

--- src/Pages/Home/Moto/Moto.js
+++ src/Pages/Home/Moto/Moto.js
@@ -3,7 +3,12 @@ import banner from '../../../images/banner/banner3.jpg'
 
 const Moto = () => {
     return (
-        
+        <div className='d-flex container my-4 justify-content-between align-items-center'>
+            <div><img src={banner} className='rounded' alt="" /></div>
+            <div className='ms-2'>
+               <h4> To keep the body in good health is a duty… otherwise we shall not be able to keep our mind strong and clear.</h4>
+            </div>
+        </div>
     );
 };
 
