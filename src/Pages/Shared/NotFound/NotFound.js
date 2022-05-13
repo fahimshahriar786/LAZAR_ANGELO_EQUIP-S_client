33e0b85fcfe1555f76803452f4ade75035ae7136
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 18:54:22 +0600

    notfound section

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,7 +3,10 @@ import notfound from '../../../images/notfound.jpg';
 
 const NotFound = () => {
     return (
-       
+        <div>
+        <h2 className='text-primary text-center'>Page Not Found !</h2>
+        <img className='w-100' src={notfound} alt="" />
+    </div>
     );
 };
 
